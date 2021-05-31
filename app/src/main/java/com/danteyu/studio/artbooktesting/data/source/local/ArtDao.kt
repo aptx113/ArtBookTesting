0ef506315c2ee8948ce26651e49d179b6b6c1669
aptx113 <aptx113@gmail.com>
2021-05-31 09:19:57 +0800

    setup room database

---
+++ app/src/main/java/com/danteyu/studio/artbooktesting/data/source/local/ArtDao.kt
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2021 Dante Yu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.danteyu.studio.artbooktesting.data.source.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Created by George Yu in May. 2021.
+ */
+@Dao
+interface ArtDao {
+
+    @Query("SELECT * FROM arts_table")
+    fun getAllArtsFlow(): Flow<List<Art>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertArt(art: Art)
+
+    @Delete
+    suspend fun deleteArt(art: Art)
+}
