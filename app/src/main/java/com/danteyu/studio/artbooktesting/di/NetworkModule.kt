afef3e5e5e006a59998447b5240d78d3646e5b2e
aptx113 <aptx113@gmail.com>
2021-05-31 17:45:10 +0800

    implement DI with Hilt

---
+++ app/src/main/java/com/danteyu/studio/artbooktesting/di/NetworkModule.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 Dante Yu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.danteyu.studio.artbooktesting.di
+
+import com.danteyu.studio.artbooktesting.BASE_URL
+import com.danteyu.studio.artbooktesting.BuildConfig
+import com.danteyu.studio.artbooktesting.data.source.api.ApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by George Yu in May. 2021.
+ */
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideMoshi() = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
+
+    @Singleton
+    @Provides
+    fun provideHttpClient() = OkHttpClient.Builder()
+        .addInterceptor(
+            HttpLoggingInterceptor().apply {
+                level =
+                    if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY
+                    else HttpLoggingInterceptor.Level.NONE
+            }
+        )
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(moshi: Moshi, client: OkHttpClient) = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .client(client)
+        .build()
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit) = retrofit.create(ApiService::class.java)
+}
